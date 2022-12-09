92c3a8a23573a25d397f9c4869fd9eb0374c02b5
javawithlwl <javawithlwl@gmail.com>
2022-12-09 06:53:29 +0530

    Added logic to print sum of two numbers

--- src/main/java/com/careerit/cj/basics/SumOfTwoNumbers.java
+++ src/main/java/com/careerit/cj/basics/SumOfTwoNumbers.java
@@ -1,4 +1,10 @@
 package com.careerit.cj.basics;
 
 public class SumOfTwoNumbers {
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    System.out.println("The sum is :" + sum);
+  }
 }
